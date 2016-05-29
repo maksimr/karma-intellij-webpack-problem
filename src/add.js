b1b2f6c5c2fefc1f090e51722f9dcc198ef07973
Maksim Ryzhikov <rv.maksim@gmail.com>
2016-05-29 23:27:39 +0300

    problem

--- src/add.js
+++ src/add.js
@@ -1,3 +1,17 @@
-module.exports = function(a, b) {
-    return a + b;
+require('util');
+require('http');
+require('url');
+
+var karmaLoad = function () {
+  require('webpack');
+  require('karma-jasmine');
+  require('babel-core');
+  require('jasmine-core');
+  require('karma');
+  require('karma-phantomjs-launcher');
+  require('karma-webpack');
+};
+
+module.exports = function (a, b) {
+  return a + b;
 };
